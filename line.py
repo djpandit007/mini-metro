d4273c1732ef84ae40b1637b1f33710027985e07
Dhananjay Pandit <dj.pandit007@gmail.com>
2020-07-27 22:14:17 -0700

    Init line class

---
+++ line.py
@@ -0,0 +1,20 @@
+from enum import Enum, unique
+
+
+@unique
+class ColorEnum(Enum):
+    BLUE = 1
+    GREEN = 2
+    RED = 3
+
+
+class Line(object):
+    def __init__(self, color):
+        try:
+            self.color = ColorEnum[color.upper()]
+        except KeyError:
+            print("Color '" + color + "' not found in ColorEnum")
+
+    def getColor(self):
+        return self.color
+
