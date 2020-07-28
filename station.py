b933b361c2ad5dd1e36283b05338404697d9a931
Dhananjay Pandit <dj.pandit007@gmail.com>
2020-07-27 22:14:00 -0700

    Init station class

---
+++ station.py
@@ -0,0 +1,19 @@
+from enum import Enum, unique
+
+
+@unique
+class ShapeEnum(Enum):
+    CIRCLE = 1
+    SQUARE = 2
+    TRIANGLE = 3
+
+
+class Station(object):
+    def __init__(self, shape):
+        try:
+            self.shape = ShapeEnum[shape.upper()]
+        except KeyError:
+            print("Shape '" + shape + "' not found in ShapeEnum")
+
+    def getShape(self):
+        return self.shape
